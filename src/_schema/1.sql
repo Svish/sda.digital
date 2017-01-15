8a12ec7e43a86f80b87111745aeaa9ebd4d191ac
Torleif Berger <torleif@outlook.com>
2017-01-15 15:38:25 +0100

    Removed not yet used tables from initial SQL

--- src/_schema/1.sql
+++ src/_schema/1.sql
@@ -3,32 +3,3 @@ ALTER DATABASE `sda_digital`
 	DEFAULT COLLATE 'utf8_danish_ci';
 
 USE `sda_digital`;
-
-
-
-CREATE TABLE IF NOT EXISTS `speaker`
-(
-	`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB;
-
-
-CREATE TABLE IF NOT EXISTS `series`
-(
-	`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB;
-
-
-CREATE TABLE IF NOT EXISTS `file`
-(
-	`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`sha256` varchar(255) NOT NULL,
-	`name` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB;
