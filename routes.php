6dad35a8961667c1f1d5e188228659422d6907c5
Torleif Berger <torleif@outlook.com>
2017-01-15 21:55:21 +0100

    Fixed main route and added possibly working one for later...

--- routes.php
+++ routes.php
@@ -6,14 +6,28 @@ return [
 	'/js/(:alpha:.js)' => 'Controller_Javascript',
 	'/theme/(:alpha:.css)' => 'Controller_Less',
 
-	# Other
+
+	# Slugs
+	'/:alpha:/:digit:/:alpha:' => function (array &$request)
+	{
+		// TODO: Does this work?
+		$params = explode('/', trim($request['path'], '/'));
+		$handler = array_shift($params);
+		$request['params'] = $params;
+
+		var_dump(get_defined_vars());
+		
+		return $handler;
+	},
+
+
+	# Any other pages
 	0 => function (array $request)
 	{
 		$h = explode('/', trim($request['path'], '/'));
-		$h = implode('_', array_map('ucfirst', $h));
-		
+		$h = 'Controller_'.implode('_', array_map('ucfirst', $h));
 		return class_exists($h)
 			? $h
 			: 'Controller_Page';
-	}
+	},
 ];
