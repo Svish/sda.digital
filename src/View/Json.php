bd7aab13d077f8ae4982c25764448aecf13b3d9e
Torleif Berger <torleif@outlook.com>
2017-02-11 23:47:56 +0100

    Validate JSONP callback

--- src/View/Json.php
+++ src/View/Json.php
@@ -13,7 +13,6 @@ class Json extends View
 		'application/javascript',	// JSONP
 		];
 
-
 	private $_data;
 
 	public function __construct($data)
@@ -31,7 +30,8 @@ class Json extends View
 			return parent::render($mime);
 
 		// Set mime if jsonp
-		if(isset($_GET['callback']))
+		$callback = $_GET['callback'] ?? false;
+		if($callback && self::validate_callback($callback))
 			$mime = $_accept[2];
 
 		// Set content-type
@@ -46,9 +46,38 @@ class Json extends View
 		$json = json_encode($this->_data, 
 			JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 
-		// TODO: Validate callback (see blog)
-		echo isset($_GET['callback'])
-			? "{$_GET['callback']}($json)"
-			: $json;
+		echo $callback
+			? "$callback($json)"
+			: "$json";
+	}
+
+
+
+	private static function validate_callback(string $callback)
+	{
+		if( ! self::is_valid_callback($callback))
+			throw new \Error\HttpException(400, 'Invalid JSONP identifier.');
+	}
+
+
+
+	/**
+	 * @see http://www.geekality.net/?p=1739
+	 */
+	private static function is_valid_callback($subject)
+	{
+		$identifier_syntax
+		= '/^[$_\p{L}][$_\p{L}\p{Mn}\p{Mc}\p{Nd}\p{Pc}\x{200C}\x{200D}]*+$/u';
+
+		$reserved_words = ['break', 'do', 'instanceof', 'typeof', 'case',
+		'else', 'new', 'var', 'catch', 'finally', 'return', 'void', 'continue', 
+		'for', 'switch', 'while', 'debugger', 'function', 'this', 'with', 
+		'default', 'if', 'throw', 'delete', 'in', 'try', 'class', 'enum', 
+		'extends', 'super', 'const', 'export', 'import', 'implements', 'let', 
+		'private', 'public', 'yield', 'interface', 'package', 'protected', 
+		'static', 'null', 'true', 'false'];
+
+		return preg_match($identifier_syntax, $subject)
+			&& ! in_array(mb_strtolower($subject, 'UTF-8'), $reserved_words);
 	}
 }
